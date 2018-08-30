25101344726a8bcb89cc67a23aa7cee3c60cb031
ogasawjk <ogasawjk@rose-hulman.edu>
2018-08-30 13:34:50 -1000

    IM DONE

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Justin Ogasawara.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -27,3 +27,31 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+Speed_Racer1 = rg.SimpleTurtle('turtle')
+Speed_Racer1.pen = rg.Pen('blue', 3)
+Speed_Racer1.speed = 10
+Speed_Racer2 = rg.SimpleTurtle('turtle')
+Speed_Racer2.pen = rg.Pen('red', 3)
+Speed_Racer2.speed = 10
+radius = 10
+for k in range(20):
+
+    Speed_Racer1.draw_circle(radius)
+    Speed_Racer2.draw_circle(radius)
+    Speed_Racer1.pen_up()
+    Speed_Racer2.pen_up()
+    Speed_Racer1.right(45)
+    Speed_Racer2.right(135)
+    Speed_Racer1.forward(8)
+    Speed_Racer2.forward(8)
+    Speed_Racer2.left(135)
+    Speed_Racer1.left(45)
+    Speed_Racer1.pen_down()
+    Speed_Racer2.pen_down()
+    radius = radius + 5
+
+window.close_on_mouse_click()
